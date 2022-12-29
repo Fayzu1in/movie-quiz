d057edb94595d4b843ec048915bad3f32fd01458
Nurbek <93489667+bugsssssss@users.noreply.github.com>
2022-12-29 23:38:42 +0500

    api improvements 1.0

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from .views import *
+from . import views, api_views
 
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    # path('', HomeView.as_view(), name='home'),
+    re_path('', api_views.QuestionListApiView.as_view()),
+
 ]
